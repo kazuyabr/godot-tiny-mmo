ca8e65aba9eabd5cce876ff52e1d9fb4a91ff86f
SlayHorizon <163869998+SlayHorizon@users.noreply.github.com>
2025-06-05 00:22:04 +0200

    Add display damage label and dummy

--- source/common/entities/gameplay/health/health_component.gd
+++ source/common/entities/gameplay/health/health_component.gd
@@ -5,7 +5,7 @@ class_name HealthComponent
 signal health_changed
 signal max_health_changed
 
-# To-do: Display damage in game like with label text -20 
+
 @export var hurtbox: Area2D
 @export var progress_bar: ProgressBar
 
@@ -35,9 +35,25 @@ func _ready() -> void:
 
 func apply_attack(attack: Attack) -> void:
 	health -= attack.damage
+	if OS.has_feature("client"):
+		display_damage(attack)
 
 
 func _on_hurt_box_area_entered(area: Area2D) -> void:
 	if area is Projectile and area.attack and area.attack.source != owner:
 		apply_attack(area.attack)
 		area.queue_free()
+
+
+func display_damage(attack: Attack) -> void:
+		var label := Label.new()
+		label.global_position = owner.global_position
+		label.text = str(attack.damage)
+		label.top_level = true
+		add_child(label)
+		var tween := create_tween()
+		tween.set_parallel()
+		tween.tween_property(label, "modulate:a",0.3, 0.7)
+		tween.tween_property(label, "scale", Vector2.ONE, 0.3)
+		tween.tween_property(label, "scale", Vector2(0.4, 0.4), 1.0).set_delay(0.6)
+		tween.chain().tween_callback(label.queue_free)
